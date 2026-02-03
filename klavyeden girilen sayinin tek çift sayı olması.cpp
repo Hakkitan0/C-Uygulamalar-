17fdd4c6dbc79a64655d9be6d92b2077e0b87bdd
Hakkı Tan <hakkitan47@gmail.com>
2026-02-04 02:40:25 +0300

    Add files via upload

---
+++ klavyeden girilen sayinin tek çift sayı olması.cpp
@@ -0,0 +1,20 @@
+/* klavyeden girilen sayinin tek/cift*/
+#include<stdio.h>
+
+int main()
+{
+	int A;
+printf("bir sayi giriniz:");
+scanf("%d",&A);
+if(A%2==0)
+{
+	printf("cift");
+}
+else
+{
+	printf("tek");
+}	
+	
+}
+
+
