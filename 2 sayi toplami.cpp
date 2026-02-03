66bbf560ff6a46087c012988fa98dd46bf95dc21
Hakkı Tan <hakkitan47@gmail.com>
2026-02-04 02:30:53 +0300

    Add files via upload

---
+++ 2 sayi toplami.cpp
@@ -0,0 +1,19 @@
+//klavyeden girilen iki sayinin toplami
+#include<stdio.h>
+
+int main()
+{
+	int sayi1,sayi2,toplam;
+	
+	printf("birinci sayiyi giriniz:");
+	scanf("%d",&sayi1);
+	
+	printf("ikinci sayiyi giriniz:");
+	scanf("%d",&sayi2);
+	
+	
+	toplam=sayi1+sayi2;
+	
+	printf("Toplama islemi sonucu =%d",toplam);
+	
+}
